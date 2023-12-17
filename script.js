e22b04e26b4847f51343b1bca8e0fa0e7a43a27c
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-17 20:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function stringChop(str, size) {
   // your code here
+	if (str == null)
+return [];
+str = String(str);
+return size > 0 ? str.match(new RegExp('.{1,' + size + '}', 'g')) : [str];
+
 }
 
 // Do not change the code below
